37def8f468e1a7fda688773041a6e81f36c22a78
Joseph Key <jkey3@brookdale.com>
2019-07-21 14:10:44 -0500

    Updates

---
+++ precache-manifest.bd35397e060229a872a9cb257e56d228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c30733df27fade7929f06b38ac8502",
+    "url": "/codecademy-ravenous/index.html"
+  },
+  {
+    "revision": "e1848819063de995dfe4",
+    "url": "/codecademy-ravenous/static/css/main.16939631.chunk.css"
+  },
+  {
+    "revision": "551d1d504404c9df217e",
+    "url": "/codecademy-ravenous/static/js/2.fa623c7d.chunk.js"
+  },
+  {
+    "revision": "e1848819063de995dfe4",
+    "url": "/codecademy-ravenous/static/js/main.5332f15a.chunk.js"
+  },
+  {
+    "revision": "f8f24dbf18b8b7870066",
+    "url": "/codecademy-ravenous/static/js/runtime~main.18ac4198.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/codecademy-ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/codecademy-ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
